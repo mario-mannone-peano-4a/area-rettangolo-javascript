1e682b7065d040854a636f7a9cf1f8b35f78e7a5
Mario Mannone <56471098+mario-mannone-peano-4a@users.noreply.github.com>
2019-11-04 19:29:59 +0100

    Commit iniziale

---
+++ AreaRettangolo.java
@@ -0,0 +1,17 @@
+package arearettangolo;
+
+/**
+ *
+ * @author mannone.mario
+ */
+public class AreaRettangolo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // avviabilità tramite pulsante "Run project"
+        new NewJFrame().setVisible(true);
+    }
+    
+}
